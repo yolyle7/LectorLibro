f49963cc45bfff962715c91d134460960b7152e7
yolyle7 <36626947+yolyle7@users.noreply.github.com>
2018-05-20 17:14:17 +0200

    vista

--- src/vista/UI.java
+++ src/vista/UI.java
@@ -1,13 +1,16 @@
 package vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 
 import control.accion.ParaUI;
@@ -15,7 +18,7 @@ import control.accion.ParaUI;
 public class UI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextArea textArea;
+	private JTextArea txtrBienvenidoAlLector;
 	private JButton btnAtras;
 	private JButton btnMarcar;
 	private JButton btnIrAMarca;
@@ -45,32 +48,40 @@ public class UI extends JFrame {
 		setTitle("Lector");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 321, 490);
-		setResizable(false);
 
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(95, 158, 160));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 
-		textArea = new JTextArea(
-				"\n\n\n\n\n      BIENVENIDO A LECTOR DE LIBROS DE YOLANDA\n\n\n\n\n\n\n VAS A DISFRUTAR DEL MARAVILLOSO LIBRO DE\n"
-						+ " JUEGO DE TRONOS, CANCION DE HIELO Y FUEGO. \n\n\n\n\n\n\n\n\n PULSA SIGUIENTE PARA CONTINUAR ",
+		txtrBienvenidoAlLector = new JTextArea(
+				"\r\n\r\n\r\n\r\n    Bienvenido al lector de libros de\n\n\n\n                           Yolanda\n\n\n\n\n\n\n\n             Pulse >> para continuar",
 				1, 1);
+		txtrBienvenidoAlLector.setFont(new Font("Eagle Lake", Font.PLAIN, 13));
+	
 		// textArea.setPreferredSize(new Dimension(1, 1));
-		textArea.setToolTipText("");
-		textArea.setLineWrap(true);
-		textArea.setWrapStyleWord(true);
-		contentPane.add(textArea, BorderLayout.CENTER);
+		txtrBienvenidoAlLector.setToolTipText("");
+		txtrBienvenidoAlLector.setLineWrap(true);
+		txtrBienvenidoAlLector.setWrapStyleWord(true);
+		contentPane.add(txtrBienvenidoAlLector, BorderLayout.CENTER);
 
 		// Panel de botones
 		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(25, 25, 112), null, null, null));
+		panel.setBackground(new Color(95, 158, 160));
 		contentPane.add(panel, BorderLayout.SOUTH);
 
 		lblNumeroPagina = new JLabel("0");
+		lblNumeroPagina.setFont(new Font("Eagle Lake", Font.BOLD, 9));
 		btnAtras = new JButton("<<");
+		btnAtras.setFont(new Font("Eagle Lake", Font.BOLD, 9));
 		btnAlante = new JButton(">>");
+		btnAlante.setFont(new Font("Eagle Lake", Font.BOLD, 9));
 		btnMarcar = new JButton("Marcar");
+		btnMarcar.setFont(new Font("Eagle Lake", Font.BOLD, 9));
 		btnIrAMarca = new JButton("Ir a");
+		btnIrAMarca.setFont(new Font("Eagle Lake", Font.BOLD, 9));
 
 		panel.add(btnAtras);
 		panel.add(btnMarcar);
@@ -80,11 +91,11 @@ public class UI extends JFrame {
 
 	}
 	public JTextArea getTextArea() {
-		return textArea;
+		return txtrBienvenidoAlLector;
 	}
 
 	public void setTextArea(JTextArea textArea) {
-		this.textArea = textArea;
+		this.txtrBienvenidoAlLector = textArea;
 	}
 
 	public JButton getBtnAtras() {
